cb0f19536f786cb2a2d5276f59c30ced56a461f0
iDinoDave <97319831+iDinoDave@users.noreply.github.com>
2023-05-03 23:25:24 -0500

    David Achachagua
    Bala enemigo 2

--- Tarea-1/Assets/Script/Enemy2.cs
+++ Tarea-1/Assets/Script/Enemy2.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy2 : Enemy
+public class Enemy2 : Enemy,IShoot
 {
     public GameObject player;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform pointShoot;
+    [SerializeField] private float timertoShoot;
+    float timer;
     public float separationDistance = 2.0f;
     NavMeshAgent agent;
 
@@ -16,6 +20,12 @@ public class Enemy2 : Enemy
 
     void Update()
     {
+        timer += Time.deltaTime;
+        if (timer >= timertoShoot)
+        {
+            timer = 0;
+            Shoot();
+        }
         Move();
     }
 
@@ -23,5 +33,10 @@ public class Enemy2 : Enemy
     {
         Vector3 targetPosition = player.transform.position - transform.forward * separationDistance;
         agent.SetDestination(targetPosition);
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bullet, pointShoot.position, Quaternion.identity);
     }
 }
