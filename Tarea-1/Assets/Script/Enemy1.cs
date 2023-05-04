1ba68ec7e1af2925012c9e8db8c5d4d94bd28c80
Gianfra100 <gianfra2400@gmail.com>
2023-05-03 21:33:26 -0500

    Gianfranco Hernandez
    Enemy 1

---
+++ Tarea-1/Assets/Script/Enemy1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy1 : Enemy
+{
+    public float maxDistance = 5f;
+    public float moveSpeed = 20f;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+        
+    public override void Move()
+    {
+        if (!agent.hasPath || agent.remainingDistance < 0.5f)
+        {
+            Vector2 randomPoint = Random.insideUnitCircle.normalized * maxDistance;
+            Vector3 targetPosition = new Vector3(randomPoint.x + transform.position.x, transform.position.y, transform.position.z);
+
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(targetPosition, out hit, 1f, NavMesh.AllAreas))
+            {
+                agent.SetDestination(hit.position);
+            }
+        }
+
+        Vector3 moveDirection = agent.desiredVelocity.normalized;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+    }
+}
